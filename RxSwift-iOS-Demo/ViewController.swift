58823b78a41ccc43354e7e9409bc80dbdd353744
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-02-21 17:38:26 +0800

    Initial Commit

---
+++ RxSwift-iOS-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwift-iOS-Demo
+//
+//  Created by Ankit Kumar on 21/02/2018.
+//  Copyright © 2018 Ankit Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

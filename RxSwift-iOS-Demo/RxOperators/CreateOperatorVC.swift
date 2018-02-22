f96369850509cb2e387faaf96d96b62d8763136b
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-02-22 11:00:42 +0800

    trying

---
+++ RxSwift-iOS-Demo/RxOperators/CreateOperatorVC.swift
@@ -0,0 +1,35 @@
+//
+//  CreateOperatorVC.swift
+//  RxSwift-iOS-Demo
+//
+//  Created by Ankit Kumar on 21/02/2018.
+//  Copyright © 2018 Ankit Kumar. All rights reserved.
+//
+
+import UIKit
+
+class CreateOperatorVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

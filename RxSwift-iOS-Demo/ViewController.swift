e1773d080331e43828652d8df14a3693230edc82
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-02-25 12:10:37 +0800

    added generate operator

--- RxSwift-iOS-Demo/ViewController.swift
+++ RxSwift-iOS-Demo/ViewController.swift
@@ -11,13 +11,14 @@ import RxSwift
 import RxCocoa
 
 class ViewController: UIViewController {
-
-   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        createOperator()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -26,12 +27,32 @@ class ViewController: UIViewController {
         switch ((sender as AnyObject).tag) {
         case 1:
             navigationController?.pushViewController(CreateOperatorVC(), animated: true)
-//            let newViewController = CreateOperatorVC()
-//            self.navigationController?.pushViewController(newViewController, animated: true)
+            //            let newViewController = CreateOperatorVC()
+        //            self.navigationController?.pushViewController(newViewController, animated: true)
         default:
             print("Nothing Selected")
         }
         
     }
+    func createOperator()  {
+        let source : Observable = Observable<Int>
+            .create { observer in
+            for i in 1...5 {
+                observer.on(.next(i))
+            }
+            observer.on(.completed)
+            
+            // Note that this is optional. If you require no cleanup you can return
+            // `Disposables.create()` (which returns the `NopDisposable` singleton)
+            return Disposables.create {
+                print("disposed")
+            }
+        }
+        
+        source.subscribe {
+            print($0)
+        }
+    }
+    
 }
 

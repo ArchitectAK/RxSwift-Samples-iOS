00bab63c50982cfad075a8804ec7e3e813fe9f8c
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-02-25 12:20:35 +0800

    updated

--- RxSwift-iOS-Demo/RxOperators/GenerateOperatorViewController.swift
+++ RxSwift-iOS-Demo/RxOperators/GenerateOperatorViewController.swift
@@ -16,6 +16,8 @@ class GenerateOperatorViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        executeGenerateOperator()
     }
 
     override func didReceiveMemoryWarning() {

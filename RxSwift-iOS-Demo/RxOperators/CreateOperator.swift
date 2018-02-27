6c27b9d0ee4931039e897ee6b04c359c2d49288e
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-02-27 17:50:55 +0800

    Added label

---
+++ RxSwift-iOS-Demo/RxOperators/CreateOperator.swift
@@ -0,0 +1,32 @@
+//
+//  CreateOperator.swift
+//  RxSwift-iOS-Demo
+//
+//  Created by Ankit Kumar on 27/02/2018.
+//  Copyright © 2018 Ankit Kumar. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+class CreateOperator {
+    func createOperator()  {
+        let source : Observable = Observable<Int>.create { observer in
+            for i in 1...5 {
+                observer.on(.next(i))
+            }
+            observer.on(.completed)
+            
+            // Note that this is optional. If you require no cleanup you can return
+            // `Disposables.create()` (which returns the `NopDisposable` singleton)
+            return Disposables.create {
+                print("disposed")
+            }
+        }
+        
+        source.subscribe {
+            print($0)
+        }
+    }
+
+}
